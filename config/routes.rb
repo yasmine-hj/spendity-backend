daa7ab9fae9d0bee6475f3295d79d68c4e9bffe8
yasmine-hj <yasminehajaji95@gmail.com>
2020-03-08 14:18:23 +0000

    namespace routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :budgets
-  resources :categories
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :categories do
+        resources :budgets
+      end
+    end
+  end
 end
