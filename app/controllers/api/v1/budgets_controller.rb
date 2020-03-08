839ae1445bdce2a7ed8fefea8d065daa2da75c58
yasmine-hj <yasminehajaji95@gmail.com>
2020-03-08 15:20:43 +0000

    add budgets controller

--- app/controllers/api/v1/budgets_controller.rb
+++ app/controllers/api/v1/budgets_controller.rb
@@ -1,3 +1,39 @@
 class Api::V1::BudgetsController < ApplicationController
+   
+    before_action :set_category
 
+    def index
+        @budgets = Budget.all
+        render json: @budgets
+      end
+    
+      def show
+        @budget = Transaction.find(params[:id])
+        render json: @budget
+      end
+    
+      def create
+        @budget = @category.Budget.new(budget_params)
+        @budget.save
+        render json: @category
+      end
+    
+      def destroy
+        @budget = Budget.find(params["id"])
+        @category = Category.find(@budget.account_id)
+        @budget.destroy
+        render json: @category
+      end
+    
+      private
+    
+      def set_category
+        @category = category.find(params[:category_id])
+      end
+    
+    
+      def budget_params
+        params.require(:budget).permit(:amount, :category_id, :currency, :notes, :start_date, :end_date)
+      end
+    
 end
