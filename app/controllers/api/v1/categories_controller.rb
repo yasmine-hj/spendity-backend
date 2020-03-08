ee8533038de484220f0e371fab5f2820f4af235d
yasmine-hj <yasminehajaji95@gmail.com>
2020-03-08 15:25:05 +0000

    add categories controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::CategoriesController < ApplicationController
+    def index
+        @categories = Category.all
+        render json: @categories
+      end
+  
+      def show
+        @category = Category.find(params[:id])
+        render json: @category
+      end
+  
+      private
+  
+      def category_params
+        params.require(:category).permit(:name, :image)
+      end
+  
 end
